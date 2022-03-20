0f82d5df82bb06fb1e59b61690c694ca9a15ee50
Everson Pereira <eversonpereiraofc@gmail.com>
2022-03-20 15:59:41 -0300

    process (STDIN/STDOUT) e módulo FS

---
+++ ex09_process.js
@@ -0,0 +1,5 @@
+process.stdout.write("Está gostando do curso?")
+process.stdin.on("data", data => {
+    process.stdout.write(`Sua resposta foi ${data.toString()}. Obrigado!`)
+    process.exit()
+})
